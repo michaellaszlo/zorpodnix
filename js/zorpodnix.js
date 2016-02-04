9df36dc1e3eec2973d0ee2edc7092f2d51ea3de5
Michael Laszlo <git@michaellaszlo.com>
2016-02-04 15:52:08 -0500

    Make and update layout in separate functions

--- js/zorpodnix.js
+++ js/zorpodnix.js
@@ -11,57 +11,74 @@ var Zorpodnix = (function () {
       canvases = {},
       contexts = {};
 
-  function makeLayout() {
+  function updateLayout() {
     var actionSize;
 
-    // Make a canvas covering the whole window.
     size.window = {
       width: window.innerWidth,
       height: window.innerHeight
     };
-    canvases.window = document.createElement('canvas');
+
+    // Full-window canvas.
     canvases.window.width = size.window.width;
     canvases.window.height = size.window.height;
     
     // Calculate the dimensions and offset of the maximal 16:9 frame.
     if (size.window.height * 16 / 9 < size.window.width) {
       // Full-height frame with margins to left and right.
-      size.frame = { height: window.size.height };
+      size.frame = { height: size.window.height };
       size.frame.width = size.frame.height * 16 / 9;
       margin.frame = { top: 0, bottom: 0 };
       margin.frame.left = (size.window.width - size.frame.width) / 2;
       margin.frame.right = margin.frame.left;
     } else {
       // Full-width frame with margins above and below.
-      size.frame = { width: window.size.width };
+      size.frame = { width: size.window.width };
       size.frame.height = size.frame.width * 9 / 16;
       margin.frame = { left: 0, right: 0 };
       margin.frame.top = (size.window.height - size.frame.height) / 2;
       margin.frame.bottom = margin.frame.top;
     }
 
-    // Divide the frame into three sections along the width: 16 = 9 + 1 + 6.
-    // These are the action area, countdown bar, and spell display.
+    // Divide the three sections along its width: 9 + 1 + 6 = 16.
+    // The height is 9:16, so the action area has sides 9:9 = square.
     actionSize = size.frame.height;
     size.action = { height: actionSize, width: actionSize };
     size.countdown = { height: actionSize, width: actionSize / 9 };
     size.spell = { height: actionSize, width: actionSize * 2 / 3 };
 
-    // Make containers for the frame and the three sections.
+    // Containers for the frame and its three sections.
     [ 'frame', 'action', 'countdown', 'spell' ].forEach(function (name) {
-      containers[name] = document.createElement('div');
       containers[name].style.width = size[name].width + 'px';
       containers[name].style.height = size[name].height + 'px';
-      containers[name].style.top = margin.frame.top + 'px';
     });
-    containers.frame.style.left = containers.action.style.left =
-        margin.frame.left + 'px';
-    containers.countdown.style.left =
-        (margin.frame.left + size.action.width) + 'px';
-    containers.spell.style.left =
-        (margin.frame.left + size.action.width + size.countdown.width) + 'px';
+    containers.frame.style.top = margin.frame.top + 'px';
+    containers.frame.style.left = margin.frame.left + 'px';
+    containers.countdown.style.left = size.action.width + 'px';
+    containers.spell.style.left = (size.action.width +
+        size.countdown.width) + 'px';
+  }
 
-    // Now that we have all the canvases, get all the contexts.
+  function makeLayout() {
+    // Full-window canvas.
+    canvases.window = document.createElement('canvas');
+    canvases.window.id = 'windowCanvas';
+    document.body.appendChild(canvases.window);
+
+    // Containers for the frame and its three sections.
+    containers.frame = document.createElement('div');
+    containers.frame.id = 'frameContainer';
+    document.body.appendChild(containers.frame);
+    [ 'action', 'countdown', 'spell' ].forEach(function (name) {
+      var container = containers[name] = document.createElement('div'),
+          canvas = canvases[name] = document.createElement('canvas');
+      container.appendChild(canvas);
+      containers.frame.appendChild(container);
+      container.id = name + 'Container';
+      canvas.id = name + 'Canvas';
+    });
+
+    // Contexts for all canvases.
     Object.keys(canvases).forEach(function (name) {
       contexts[name] = canvases[name].getContext('2d');
     });
@@ -69,6 +86,8 @@ var Zorpodnix = (function () {
 
   function load() {
     makeLayout();
+    updateLayout();
+    window.onresize = updateLayout;
   }
 
   return {
